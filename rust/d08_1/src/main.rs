c72a9ae38476023c581d23335dbb462c13f3b840
Auroberfroid <aurelien.roberfroid@gmail.com>
2024-07-27 19:21:44 +0200

    d08 P1 done rust

--- rust/d08_1/src/main.rs
+++ rust/d08_1/src/main.rs
@@ -1,186 +1,173 @@
-use std::{env::current_dir, fmt::{Formatter, Result as FmtResult, Display}, fs::read_to_string, path::PathBuf};
+use std::{env::current_dir, fs::read_to_string, path::PathBuf, collections::HashMap};
 
-const HASH_TABLE_SIZE: usize = 678; 
-
-fn debug_print<T: Display>(arg: T) {
-    println!("ARO DEBUG: {arg}");
-}
-
-enum Direction {
-    Right,
-    Left,
-    Unknown
-}
-
-impl Display for Direction {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        match *self {
-            Direction::Right => write!(f, "Right"),
-            Direction::Left => write!(f, "Left"),
-            Direction::Unknown => write!(f, "Unknown")
-        }
-    }
-}
-
+const DEBUG: bool = false;
 
 struct Node {
-    id: [char; 3],
-    left: [char; 3],
-    right: [char; 3]
+    id: String,
+    left: String,
+    right: String
 }
 
 impl Node {
-    fn get_as_str(&self, attrib: &str) -> String {
-        let mut res: String = String::new();
-        match attrib {
-            "id" => {
-                for c in self.id {
-                    res.push(c);
-                }
-            }
-            "left" => {
-                for c in self.left {
-                    res.push(c);
-                }
-            }
-            "right" => {
-                for c in self.right {
-                    res.push(c);
-                }
-            }
-            _ => {
-                eprintln!("Invalid attrib: {}", attrib);
-                res.push('N');
-                res.push('O');
-            }
-        }
-        res
-    }
-}
-
-impl Display for Node {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        write!(f, "Node {} => ({}, {})", self.get_as_str("id"), self.get_as_str("left"), self.get_as_str("right"))
-    }
-}
-
-struct BoostedString(String);
-
-impl Node {
-    fn new(id: [char; 3], left: [char; 3], right: [char; 3]) -> Self {
+    fn new(id: &str, left: &str, right: &str) -> Self {
         Self {
-            id,
-            left,
-            right
+            id: id.to_string(),
+            left: left.to_string(),
+            right: right.to_string()
         }
     }
 }
 
-impl Into<[char; 3]> for BoostedString {
-    fn into(self) -> [char; 3] { 
-        let mut res: [char; 3] = ['.'; 3];
-        for (i, iter_char) in self.0.chars().enumerate() {
-            res[i] = iter_char;
-            if i == 2 {break}
-        }
-        res
-    }
+#[derive(Debug)]
+enum Direction {
+    Right,
+    Left
 }
 
-fn id_converter(x: &str) -> [char; 3] {
-    let mut res: [char; 3] = ['.'; 3];
-        for (i, iter_char) in x.chars().enumerate() {
-            res[i] = iter_char;
-            if i == 2 {break}
-        }
-        res
-}
 
-fn get_input(filename: &str) -> Result<(Vec<Node>, Vec<Direction>), String> {
-    let input_dir = "files";
+/// Parse the input of the AOC D8 into a HashMap of Nodes and a Vec of Directions
+fn get_input(filename: &str) -> Result<(HashMap<String, Node>, Vec<Direction>), String> {
+    
+    // Returned processed input
+    let mut directions = Vec::<Direction>::new();
+    let mut nodes: HashMap<String, Node> = HashMap::new();
+
+
+    // Open the file
     let filepath: PathBuf;
-
+    let input_dir = "files";
     match current_dir() {
         Ok(val) => {
             filepath = val.join(input_dir).join(filename);
         }
         Err(error) => {
-            let err_msg = format!("[Error while getting the current directory. Trace: {error}]");
+            let err_msg = format!("[Error while getting the current directory: {error}]");
             return Err(err_msg);
         }
     }
 
+    // Get the content 
     let content: String;
     match read_to_string(filepath) {
         Ok(val) => {
             content = val;
         }
         Err(error) => {
-            let err_msg = format!("[Error while reading the file content. Trace: {error}]");
-            return Err(err_msg);
+            let err_msg = format!("[Error while getting the file content: {error}]");
+            return Err(err_msg); 
         }
-
     }
 
-    let mut directions = Vec::<Direction>::new();
-    let mut nodes = Vec::<Node>::new();
-    for (index_line, line) in content.split('\n').enumerate() {
-        if index_line == 0 {
-            for direction_char in line.chars() {
-                match direction_char {
-                    'R' => {
-                        directions.push(Direction::Right);
-                    }
+    // Iterate over lines 
+    for (line_index, line) in content.lines().enumerate() {
+        // Process the directions
+        if line_index == 0 {
+            for dir in line.chars() {
+                match dir {
                     'L' => {
                         directions.push(Direction::Left);
                     }
+                    'R' => {
+                        directions.push(Direction::Right);
+                    }
                     _ => {
-                        let err_msg = format!("[Error while processing the direction. Trace: Unknown direction character parsed]");
+                        let err_msg = "Error while getting the file content".to_string();
                         return Err(err_msg);
                     }
                 }
             }
-            continue;
         }
-        if line != "" {
-            let equal_split = line.split('=').map(|x| x.to_owned()).collect::<Vec<String>>();
-            let id: [char; 3] = id_converter(&equal_split[0]);
-            
-            
-            let coma_split = equal_split[1].split(',').map(|x| x.to_owned()).collect::<Vec<String>>();
-            // debug_print(&coma_split[0]);
-            let left: [char; 3] = id_converter(coma_split[0].replace("(", "").trim());
-            let right: [char; 3] = id_converter(coma_split[1].replace(")", "").trim());
-            nodes.push(Node::new(id, left, right));
+        // Skip the line 1 and process the other as Nodes
+        // AAA = (BBB, CCC)
+        else if line_index > 1 {
+            let mut id: &str = "000";
+            let mut left: &str = "000";
+            let mut right: &str = "000";
+            for (eq_split_index, eq_split) in line.split('=').enumerate() {
+                if eq_split_index == 0 {
+                    id = eq_split.trim();
+                }
+                else {
+                    for (comma_eq_split_index, comma_eq_split) in eq_split.split(',').enumerate() {
+                        if comma_eq_split_index == 0 {
+                            left = comma_eq_split.trim().trim_matches('(').trim();
+                        }
+                        else {
+                            right = comma_eq_split.trim().trim_matches(')').trim();
+                        }
+                    }
+                }
+            }
+            // Insert nodes into the HashMap
+            let node = Node::new(id, left, right);
+            nodes.insert(node.id.clone(), node);
         }
     }
+
     Ok((nodes, directions))
 }
 
-fn main() -> Result<(), String>{
-    let filename: &str = "input.txt";
 
-    let mut nodes: Vec<Node>;
+/// Compute the required amount of steps to get to the ending node from the starting node
+fn compute_steps(starting_node_id: &str, ending_node_id: &str, node_hashmap: &HashMap<String, Node>, directions: &Vec<Direction>) -> usize {
+    let mut count: usize = 0;
+    let end_node = node_hashmap.get(ending_node_id).unwrap();
+    let mut current_node = node_hashmap.get(starting_node_id).unwrap();
+
+    loop {
+        for dir in directions {
+            match dir {
+                &Direction::Left => {
+                    current_node = node_hashmap.get(&current_node.left).unwrap();
+                }
+                &Direction::Right => {
+                    current_node = node_hashmap.get(&current_node.right).unwrap();
+                }
+            }
+            count += 1;
+            if current_node.id == ending_node_id {
+                return count;
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), i8> {
+    let filename: &str;
+    if DEBUG {
+        filename = "input_debug.txt";
+    }
+    else {
+        filename = "input.txt";
+    }
+
     let mut directions: Vec<Direction>;
+    let mut nodes: HashMap<String, Node>;
+
     match get_input(filename) {
-        Ok(val) => {
-            nodes = val.0;
-            directions = val.1;
+        Ok((val_nodes, val_directions)) => {
+            nodes = val_nodes;
+            directions = val_directions;
+            println!("Get input success");
+
         }
         Err(error) => {
-            let err_msg = format!("[Error while getting the input: {error}]");
-            eprintln!("{err_msg}");
-            return Err(err_msg);
+            let err_msg = format!("[Error while get_input. Error: {error}]");
+            eprintln!("{}", err_msg);
+            return Err(-1);
         }
     }
 
-    for direct in directions {
-        print!("{direct} ");
-    }
-    println!("\n######################");
-    for node in nodes {
-        println!("{node}");
-    }
+    // Check print
+    // nodes.get("AAA").map(|node: &Node| println!("Node id: {}, left: {}, right: {}", node.id, node.left, node.right));
+    // println!("Directions: {:?}", directions);
+
+    let starting_node_id = "AAA";
+    let ending_node_id = "ZZZ";
+
+    let res = compute_steps(starting_node_id, ending_node_id, &nodes, &directions);
+
+    println!("The result is {}", res);
 
     Ok(())
 }
